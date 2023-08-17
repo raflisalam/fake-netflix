550c6ab910dac0987f375fa385f5b06ec7e0af0e
raflisalam <raflisalam.dokumen@gmail.com>
2023-08-17 13:14:31 +0800

    [setup] Add libraries and dependencies used

---
+++ app/src/main/java/com/raflisalam/fakeneflix/FakeNetflixApplication.kt
@@ -0,0 +1,4 @@
+package com.raflisalam.fakeneflix
+
+class FakeNetflixApplication {
+}
---
+++ app/src/main/java/com/raflisalam/fakeneflix/common/Status.kt
@@ -0,0 +1,7 @@
+package com.raflisalam.fakeneflix.common
+
+sealed class Status<T> (val data: T? = null, val message: String? = null) {
+    class Success<T> (data: T): Status<T>(data)
+    class Error<T> (message: String, data: T? = null): Status<T>(data, message)
+    class Loading<T> (data: T? = null): Status<T>(data)
+}
